3ae9ae74a02da5ae7e72954a35a40c19f2248d16
danial <danialabdl90@gmail.com>
2023-04-24 20:39:21 +0330

    Update Hero.gd

--- project1/Classes/Hero.gd
+++ project1/Classes/Hero.gd
@@ -1,1 +1,12 @@
+class_name Hero
+
+var xp: int
+var level : int
+var strength : int
+var stamina:int
+var speed:int
+var agility:int
+var items:Array[Item] 
+var current_weapon : Weapon
+var current_skill : Skill
 
