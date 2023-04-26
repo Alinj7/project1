e7f33ec92a9edb377d3c1dfeaf0b51b1b77908e5
danial <danialabdl90@gmail.com>
2023-04-26 11:39:16 +0330

    Create fight_arena.gd

---
+++ project1/Scripts/fight_arena.gd
@@ -0,0 +1,12 @@
+extends Node2D
+class_name Fight_arena
+
+var turn:int #0 and 1
+var hero:fighter
+var enemy:fighter
+var fighters:Array(fighter)
+var active_fighter:fighter
+
+
+func change_turn()
+func play_turn()
