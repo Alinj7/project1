c6b43c1686a03de4e981d0ebf5ac05170316a16d
danial <danialabdl90@gmail.com>
2023-04-26 11:30:45 +0330

    Create fighter.gd

---
+++ project1/Scripts/fighter.gd
@@ -0,0 +1,16 @@
+extends Area2D
+class_name Fighter
+
+var cls=Hero.new()
+var strength
+var hp
+var speed
+var agility
+var weapon:Weapon
+var skill:Skill
+var is_active_fighter:bool
+var anim=$animatedSprite
+var attach_point=$attach_point
+
+func play_turn()
+func attack()
