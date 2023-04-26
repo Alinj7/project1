1ddbe762477f9adfe65a3d9a6c2637f9d6576679
danial <danialabdl90@gmail.com>
2023-04-26 17:15:14 +0330

    Update Item.gd

--- project1/Classes/Item.gd
+++ project1/Classes/Item.gd
@@ -3,4 +3,13 @@ class_name Item
 var name:String
 var type =["Strength",  "Speed", "Hp" , "Agility"] 
 
-var increment:int #1 to 5
+
+func _init(mType, mValue):
+    mValue=value
+    #todo
+
+var value:int #1 to 5
+
+
+func add_value(stat):
+    stat+=value
