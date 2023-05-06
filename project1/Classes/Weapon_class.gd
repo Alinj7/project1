7013e5fd6f8e22565a2df46d5d3d0708f256a288
danial <danialabdl90@gmail.com>
2023-05-06 12:40:26 +0330

    Add files via upload

--- project1/Classes/Weapon_class.gd
+++ project1/Classes/Weapon_class.gd
@@ -3,9 +3,14 @@ class_name Weapon
 var title : String
 var damage : int
 var level : int
+var speed_factor :int =0
+var strength_factor:int = 0
+var agility_factor:int = 0
 
-
-func _init(name="knife",dmg=5,lvl=1):
+func _init(name="knife",dmg=5,lvl=1, speed_f=0, strength_f=0,agility_f=0):
 	title = name
 	damage = dmg
 	level = lvl
+	speed_factor = speed_f
+	strength_factor = strength_f
+	agility_factor = agility_f
